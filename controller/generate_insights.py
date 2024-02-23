6922ab42a8b9ab14175fbf937950b1303a2dd39f
Junaid shah <122867939+junaidshah786@users.noreply.github.com>
2024-02-23 17:31:27 +0530

    Update generate_insights prompt .py

--- controller/generate_insights.py
+++ controller/generate_insights.py
@@ -14,16 +14,11 @@ openai.api_key = os.getenv('openai_api_key')
 
 def make_prompt(user_query,data):
     prompt = f"""
-                You are an expert Data Analyst.\
                 Your role is to interpret and analyze the data in alignment with the requirements specified in the user query.\
                 The response should not hold any specification of query and structure of data just the conclusion from the data\
-                Provide a precise response.\
-             
-                        
+                Provide a precise response.\      
 
-
-
-        ```{user_query}``` 
+        ```{sql_query}``` 
         ```{data}```       
         
     """
@@ -40,14 +35,14 @@ def get_completion(messages, model="gpt-3.5-turbo"):
 def get_insights(sql_query,data):
 
     prompt = make_prompt(sql_query,data)
-    messages = [
-      
-        {"role": "system", "content": prompt}
-    ] 
     # messages = [
-    #     {"role": "system", "content": "Do not use any preamble."},
-    #     {"role": "user", "content": prompt}
+      
+    #     {"role": "system", "content": prompt}
     # ] 
+    messages = [
+        {"role": "system", "content": "You are an expert Data Analyst."},
+        {"role": "user", "content": prompt}
+    ] 
 
     response = get_completion(messages)
     
